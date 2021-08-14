f528896799e10aee769116d18dc1ef5e060de016
Bishnu Prasad Chowdhury <bishnucit@gmail.com>
2021-08-14 10:50:16 +0530

    Update readProperties.py

--- POM_FRAMEWORK/utilities/readProperties.py
+++ POM_FRAMEWORK/utilities/readProperties.py
@@ -21,3 +21,18 @@ class ReadConfig:
     def getPass():
         password = config.get('common data', 'password')
         return password
+
+    @staticmethod
+    def getFirstName():
+        firstname = config.get('common data', 'firstname')
+        return firstname
+
+    @staticmethod
+    def getLastName():
+        lastname = config.get('common data', 'lastname')
+        return lastname
+
+    @staticmethod
+    def getPostal():
+        postal = config.get('common data', 'postal')
+        return postal
